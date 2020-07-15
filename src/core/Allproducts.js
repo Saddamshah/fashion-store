49634ede4d4bd4223ee0c8ca541539dc697b0e04
Saddam Shah <shahsaddam103@gmail.com>
2020-07-15 17:33:21 +0530

    added loader in All Products Page

--- src/core/Allproducts.js
+++ src/core/Allproducts.js
@@ -3,19 +3,22 @@ import '../styles.css'
 import Base from './Base'
 import Card from './Card'
 import { getProducts } from './helper/coreapicalls'
+import { Circular } from 'styled-loaders-react'
 
 
 export default function AllProducts() {
 
     const [products, setProducts] = useState([])
     const [error, setError] = useState(false)
-    
+    const [loading, setLoading] = useState(true)
+
     const loadAllProducts = () => {
         getProducts().then(data => {
             if (data.error) {
                 setError(data.error)
             } else {
                 setProducts(data)
+                setLoading(false)
             }
         })
     }
@@ -27,8 +30,9 @@ export default function AllProducts() {
 
     return (
         <Base className="container-fluid" >
-           
-               <div className="whiteBG">
+            {loading ? <div className="loadingStyle">{<Circular color="#172337" size="65px" />}</div> :
+
+                <div className="whiteBG">
                     <div className="container-fluid  pt-4 pb-1 px-4  my-3">
                         <h2 className="text-center heading text-capitalize pb-2">All of Our Products</h2>
                         <h5 className="heading text-center pb-3">Total {products.length}</h5>
@@ -36,18 +40,19 @@ export default function AllProducts() {
                     </div>
                     <div className="container-fluid ">
                         <div className="row">
-                        { products.map((product, index) => {
-                            return (
-                                <>
-                                    <div key={index} className="mx-auto mb-2">
-                                        <Card product={product}/>
-                                    </div> 
-                                </>
-                            )
-                        })}
-                    </div>
+                            {products.map((product, index) => {
+                                return (
+                                    <>
+                                        <div key={index} className="mx-auto mb-2">
+                                            <Card product={product} />
+                                        </div>
+                                    </>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
+            }
         </Base>
     )
 }
